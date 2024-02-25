f035e2a1c31d6114cfcabe939ad9a677d1b7637d
Vitor Valerio Hoffmann <vitorvaleriohoffmann@gmail.com>
2024-02-25 18:27:18 -0300

    Visualizar clientes e excluir clientes no front

--- docpro-node/index.js
+++ docpro-node/index.js
@@ -1,30 +1,38 @@
 const { Sequelize } = require('sequelize')
-const sequelize = new Sequelize({
+const express = require('express')
+const cors = require('cors')
+
+const app = new express()
+app.use(cors())
+app.use(express.json())
+
+//use npm run dev
+
+const sequelize = new Sequelize({ // define o login no db
     dialect: 'mysql',
     host: '35.193.251.32',
     username: 'myuser',
     password: '123456',
     database: 'dw-docpro',
-  });
+});
 
-sequelize.authenticate().then(function(){
+sequelize.authenticate().then(function(){ // faz a verificação do login no db
     console.log("Conectado ao Banco de Dados")
 }).catch(function(erro){
     console.log(erro)
 })
-const express = require('express')
 
-const app = new express()
-
-app.get('/', (req,res) => {
-    sequelize.query('SELECT * FROM Clientes', { type: Sequelize.QueryTypes.SELECT })
-  .then(results => {
-    res.json(results);
+app.get('/dados', (req, res) => {
+  sequelize.query('SELECT * FROM Clientes', { type: Sequelize.QueryTypes.SELECT }) // recebe os dados do db
+  .then(result => {
+    res.json(result)
   })
-  .catch(error => {
-    res.status(500).send('Erro na consulta:', error);
-  });
-})
+});
+
+app.delete('/dados/:id', (req, res) => {
+  const id = req.params.id;
+  sequelize.query(`DELETE FROM Clientes WHERE id = ${id}`, { type: Sequelize.QueryTypes.DELETE }) // recebe o id a partir do reacjs e deleta no db de acordo com o id
+});
 
 app.listen(3030, () => {
     console.log("Local Server Iniciado")
